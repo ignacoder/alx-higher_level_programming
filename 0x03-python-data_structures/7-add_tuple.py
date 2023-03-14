b36cab0368295b6585f8ebe4a3fb6e590249da95
Ignatius Okon <ignaokon@yahoo.com>
2023-03-13 17:08:27 -0700

    python-data_structures

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -13,5 +13,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
         else:
             tuple_b = tuple_b[0], 0
 
-    added_tuple = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
-    return 
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
